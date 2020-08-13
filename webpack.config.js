ce5bfc1dd82b9fd3414786a0ede074583b952ba4
Mjeru <65415878+Mjeru@users.noreply.github.com>
2020-08-13 23:51:25 +0700

    bundle path in html

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,40 @@
-const path = require('path');
+const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-
-
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    plugins: [new ExtractTextPlugin("bundle.css")],
-    module: {
-        rules: [{
-                test: /\.css$/i,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader"
-                }),
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env',"@babel/preset-react"]
-                    }
-                }
-            },
-
-        ],
-
-    },
-    devServer: {
-        historyApiFallback: true,
-        contentBase: path.join(__dirname, '/'),
-        compress: true,
-        port: 9000
-      }
-
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	plugins: [new ExtractTextPlugin("bundle.css")],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: "css-loader",
+				}),
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"],
+					},
+				},
+			},
+		],
+	},
+	devServer: {
+		
+		historyApiFallback: true,
+		contentBase: path.join(__dirname, "/"),
+		compress: true,
+		hot:true,
+		port: 9000,
+	},
 };
